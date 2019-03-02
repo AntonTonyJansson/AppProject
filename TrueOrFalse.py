6ffc89704eb6ddb1ebbf2dc381ec6779ce665110
Herman <herman.jansson.mail@gmail.com>
2019-03-02 15:34:07 +0100

    hejhejhej

--- TrueOrFalse.py
+++ TrueOrFalse.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-# Testing new branch, second try
+# Testing new branch, third try
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import os
